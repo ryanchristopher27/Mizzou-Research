131a4b765024c61b8f1cb7540c50e74003a45d1a
Ryan Christopher <ryandchristopher27@gmail.com>
2024-02-09 12:10:47 -0600

    vit working on personal cuda env

--- src/torch_code/Vit_Kfold_UCMerced.py
+++ src/torch_code/Vit_Kfold_UCMerced.py
@@ -11,7 +11,7 @@ import torchvision.datasets as datasets
 from torch.utils.data import random_split
 from torchvision.transforms import transforms
 import numpy as np
-from tqdm import tqdm as progressbar
+from tqdm import tqdm
 from tqdm import trange
 from skimage.feature import (
     local_binary_pattern as lbp,
@@ -22,12 +22,13 @@ from torchvision.transforms.functional import to_tensor
 import cv2 as cv
 from sklearn import metrics
 from torchvision.transforms.functional import normalize
+from sklearn.metrics import precision_recall_fscore_support as evaluate
 
 from utils import *
 
 # GLOBAL VARIABLES
 TORCH_NUM_JOBS = int(os.environ.get("TORCH_NUM_JOBS", "4"))
-TORCH_NUM_EPOCHS = int(os.environ.get("TORCH_NUM_EPOCHS", "1"))
+TORCH_NUM_EPOCHS = int(os.environ.get("TORCH_NUM_EPOCHS", "20"))
 
 # Function to preprocess images
 def preprocess_images(images):
@@ -72,7 +73,10 @@ def get_data(train_batch_size, test_batch_size) -> ():
 def main():
     cuda_setup()
 
-    train_data_loader, test_data_loader = get_data(train_batch_size=16, test_batch_size=16)
+    train_batch_size = 16
+    test_batch_size = 16
+
+    train_data_loader, test_data_loader = get_data(train_batch_size=train_batch_size, test_batch_size=test_batch_size)
 
     # num_classes = train_data_loader.dataset.num_classes
 
@@ -91,12 +95,12 @@ def main():
     model.cuda()
     model.train()
 
-    for epoch in trange(TORCH_NUM_EPOCHS):
+    for epoch in range(TORCH_NUM_EPOCHS):
         print("===" * 30 + f"\nEpoch [{epoch+1} / {TORCH_NUM_EPOCHS}]")
         epoch_loss = 0
         epoch_correct = 0
 
-        for batch_idx, (images, labels) in enumerate(train_data_loader):
+        for i, (images, labels) in tqdm(enumerate(train_data_loader), total=len(train_data_loader)):
             images = images.cuda()
             labels = labels.cuda()
 
@@ -110,18 +114,18 @@ def main():
             loss.backward()
             optimizer.step()
 
-            _, predictions = torch.max(model_outputs, 1)
+            _, predictions = torch.max(outputs, 1)
             epoch_correct += torch.sum(predictions == labels)
             epoch_loss += loss.item()
 
-            if i > 0 and (i % (len(train_data_loader) // 10) == 0 or i == 1):
-                print(f"{i} / {len(train_data_loader)}" + 
-                    f"\tLoss = {epoch_loss / i:.2f}" + 
-                    f"\tAcc = {epoch_correct:d} / {i * train_data_loader.batch_size} " + 
-                    f"({epoch_correct / (i * train_data_loader.batch_size) * 100:.1f}%)", flush=True)
+            # if i > 0 and (i % (len(train_data_loader) // 10) == 0 or i == 1):
+            #     print(f"{i} / {len(train_data_loader)}" + 
+            #         f"\tLoss = {epoch_loss / i:.2f}" + 
+            #         f"\tAcc = {epoch_correct:d} / {i * train_data_loader.batch_size} " + 
+            #         f"({epoch_correct / (i * train_data_loader.batch_size) * 100:.1f}%)", flush=True)
 
         print(f"Loss = {epoch_loss / len(train_data_loader):.4f}")
-        print(f"Train Acc = {epoch_correct / len(cifar10_train) * 100:.2f}%")
+        print(f"Train Acc = {epoch_correct / (len(train_data_loader) * train_batch_size) * 100:.2f}%")
             
 
     model.eval()
@@ -130,7 +134,7 @@ def main():
     labels = []
     with torch.no_grad():
         print("---" * 30 + f"\nRunning Eval")
-        for idx, (images, lb) in tqdm(enumerate(test_data_loader), total=len(test_data_loader)):
+        for i, (images, lb) in tqdm(enumerate(test_data_loader), total=len(test_data_loader)):
 
             model_outputs = model(images.cuda())
 
@@ -138,12 +142,14 @@ def main():
 
             labels.extend(lb.numpy().tolist())
             predictions.extend(preds.cpu().numpy().tolist())
-            if i > 0 and i % (len(test_data_loader) // 10) == 0:
-                print(f"{i} / {len(test_data_loader)}", flush=True)
+            # if i > 0 and i % (len(test_data_loader) // 10) == 0:
+            #     print(f"{i} / {len(test_data_loader)}", flush=True)
 
-    prec, rec, fscore, _ = evaluate(predictions, labels, average="macro")
+    acc = metrics.accuracy_score(predictions, labels)
+    prec, rec, fscore, _ = evaluate(predictions, labels, average="macro", zero_division=0)
 
     print("*" * 20 + f"""\n
+    Accuracy  \t{acc*100:.2f}%
     Precision  \t{prec*100:.2f}%
     Recall  \t{rec*100:.2f}%
     F-1 Score \t{fscore*100:.2f}%
